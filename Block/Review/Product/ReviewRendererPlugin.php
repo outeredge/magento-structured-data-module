d2c18ed51bab123ac7494307faeb5c1927368660
Michael Windell <mike@outeredgeuk.com>
2017-07-28 16:24:39 +0100

    remove magento default structured markup

---
+++ Block/Review/Product/ReviewRendererPlugin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OuterEdge\StructuredData\Block\Review\Product;
+
+use Magento\Review\Block\Product\ReviewRenderer;
+use DOMDocument;
+use DOMXPath;
+
+class ReviewRendererPlugin
+{
+    public function afterGetReviewsSummaryHtml(ReviewRenderer $subject, $html)
+    {
+        $dom = new DOMDocument();
+        $dom->loadHTML($html);
+        $xpath = new DOMXPath($dom);
+        foreach (['itemprop', 'itemtype', 'itemscope'] as $schemaAttribute) {
+            $nodes = $xpath->query('//*[@' . $schemaAttribute . ']');
+            foreach ($nodes as $node) {
+                $node->removeAttribute($schemaAttribute);
+            }
+        }
+        return $dom->saveHTML();
+    }
+}
