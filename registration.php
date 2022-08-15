43122454eaab24352a7b9a4bc24624209063710d
David Windell <david@outeredgeuk.com>
2022-08-15 17:16:39 +0100

    Fix DI compile error

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 
-if (class_exists(\Magento\Framework\Component\ComponentRegistrar::class)) {
+if (class_exists('Magento\Framework\Component\ComponentRegistrar')) {
     \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
         'OuterEdge_StructuredData',
