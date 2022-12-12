c3700e13065e8b91e547ebd6b38f41293de29c8f
Bruno Dias <ibroopi@gmail.com>
2022-12-12 12:25:44 +0000

    Fix review schema on product page

---
+++ Plugin/StripReviewSummary.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OuterEdge\StructuredData\Plugin;
+
+use Magento\Review\Block\Product\ReviewRenderer;
+
+class StripReviewSummary
+{
+    public function afterGetReviewsSummaryHtml(ReviewRenderer $subject, $result)
+    {
+        $dom = new \DOMDocument;
+        $dom->loadHTML($result);
+        $xpath = new \DOMXPath($dom);
+        $nodes = $xpath->query("//@itemprop|//@itemscope|//@itemtype");
+        foreach ($nodes as $node) {
+            $node->parentNode->removeAttribute($node->nodeName);
+        }
+
+        return $dom->saveHTML();
+    }
+}
