c1b4796e25e0aa41a45ab237ab669fa3fe707048
Victor Seager <vseager@gmail.com>
2023-06-27 15:27:04 +0100

    Don't throw PHP error on older versions if review content contains HTML5 tags

--- Plugin/StripReviewSummary.php
+++ Plugin/StripReviewSummary.php
@@ -12,14 +12,18 @@ class StripReviewSummary
             return $result;
         }
         
-        $dom = new \DOMDocument;
-        $dom->loadHTML($result);
-        $xpath = new \DOMXPath($dom);
-        $nodes = $xpath->query("//@itemprop|//@itemscope|//@itemtype");
-        foreach ($nodes as $node) {
-            $node->parentNode->removeAttribute($node->nodeName);
+        try {
+            $dom = new \DOMDocument;
+            $dom->loadHTML($result);
+            $xpath = new \DOMXPath($dom);
+            $nodes = $xpath->query("//@itemprop|//@itemscope|//@itemtype");
+            foreach ($nodes as $node) {
+                $node->parentNode->removeAttribute($node->nodeName);
+            }
+    
+            return $dom->saveHTML();
+        } catch(\Exception $e) {
+            return $result;
         }
-
-        return $dom->saveHTML();
     }
 }
