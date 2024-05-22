32370ff04b8b0325acca776ad714ec7203456254
Bruno Dias <ibroopi@gmail.com>
2024-05-22 10:42:05 +0100

    Fix typo

--- Observer/FlushStructuredDataCache.php
+++ Observer/FlushStructuredDataCache.php
@@ -20,7 +20,7 @@ class FlushStructuredDataCache implements ObserverInterface
         $object = $observer->getEvent()->getObject();
 
         if ($object instanceof Product && $object->hasDataChanges()) {
-            $cacheId = StructuredDataCache::TYPE_IDENTIFIER .'_'. str_replace(' ', '_', $sku);
+            $cacheId = StructuredDataCache::TYPE_IDENTIFIER .'_'. str_replace(' ', '_', $object->getSku());
             $this->cache->remove($cacheId);
         }
     }
