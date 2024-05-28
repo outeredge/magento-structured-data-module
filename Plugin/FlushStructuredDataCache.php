535e8167ce086965204a2fd22e01fdfb86d453ae
Bruno Dias <ibroopi@gmail.com>
2024-05-28 16:43:14 +0100

    Fix flush cache, changed to beforePlugin

--- Plugin/FlushStructuredDataCache.php
+++ Plugin/FlushStructuredDataCache.php
@@ -3,28 +3,22 @@
 namespace OuterEdge\StructuredData\Plugin;
 
 use OuterEdge\StructuredData\Model\Cache\Type\StructuredDataCache;
-use Magento\Framework\App\CacheInterface;
 use Magento\Framework\App\Cache;
 
 class FlushStructuredDataCache
 {
     private $tagsList = ['cat_p_', 'catalog_product_'];
 
-    public function __construct(
-        protected CacheInterface $cache
-    ) {
-    }
-
-    public function afterClean(Cache $subject, $result, $tags)
+    public function beforeClean(Cache $subject, $tags = [])
     {
         foreach($tags as $tag) {
             $tagName = preg_replace('/\d/', '', $tag);
             if (in_array($tagName, $this->tagsList)) {
                 $prodId = str_replace($tagName, '', $tag);
                 $cacheId = StructuredDataCache::TYPE_IDENTIFIER .'_'. $prodId;
-                $this->cache->remove($cacheId);
+                $subject->remove($cacheId);
             }
         }
-        return $result;
+        return [$tags];
     }
 }
